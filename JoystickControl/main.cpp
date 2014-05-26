6659942ec7cbf2c95f37aa7d32eb038d81d0b08b
Jose Luis Blanco <joseluisblancoc@gmail.com>
2014-05-26 13:01:56 +0200

    Templatized main.cpp's

--- JoystickControl/main.cpp
+++ JoystickControl/main.cpp
@@ -1,51 +1,11 @@
 /* +---------------------------------------------------------------------------+
    |                 Open MORA (MObile Robot Arquitecture)                     |
-   |                                                                           |
-   |                        http://babel.isa.uma.es/mora/                      |
-   |                                                                           |
-   |   Copyright (C) 2010  University of Malaga                                |
-   |                                                                           |
-   |    This software was written by the Machine Perception and Intelligent    |
-   |      Robotics (MAPIR) Lab, University of Malaga (Spain).                  |
-   |    Contact: Jose-Luis Blanco  <jlblanco@ctima.uma.es>                     |
-   |                                                                           |
-   |  This file is part of the MORA project.                                   |
-   |                                                                           |
-   |     MORA is free software: you can redistribute it and/or modify          |
-   |     it under the terms of the GNU General Public License as published by  |
-   |     the Free Software Foundation, either version 3 of the License, or     |
-   |     (at your option) any later version.                                   |
-   |                                                                           |
-   |   MORA is distributed in the hope that it will be useful,                 |
-   |     but WITHOUT ANY WARRANTY; without even the implied warranty of        |
-   |     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         |
-   |     GNU General Public License for more details.                          |
-   |                                                                           |
-   |     You should have received a copy of the GNU General Public License     |
-   |     along with MORA.  If not, see <http://www.gnu.org/licenses/>.         |
-   |                                                                           |
    +---------------------------------------------------------------------------+ */
 
+#include "CJoystickApp.h" // User C++ app class 
 
-#include "CJoystickApp.h"
+#define MORA_APP_CLASS         CJoystickApp     
+#define MORA_APP_NAME          "JoystickControl"    // Default MOOSApp app name
 
-int main(int argc ,char * argv[])
-{
-    const char * sMissionFile = "Mission.moos";
-    const char * sMOOSName = "pJoystickControl";
-
-
-    switch(argc)
-    {
-    case 3:
-        sMOOSName = argv[2];
-    case 2:
-        sMissionFile = argv[1];
-    }
-
-    CJoystickApp TheApp;
-    TheApp.Run(sMOOSName,sMissionFile);
-
-    return 0;
-}
+#include <mora_main.h>
 
--- Localization2D_PF/main.cpp
+++ Localization2D_PF/main.cpp
@@ -1,63 +1,11 @@
 /* +---------------------------------------------------------------------------+
    |                 Open MORA (MObile Robot Arquitecture)                     |
-   |                                                                           |
-   |                        http://babel.isa.uma.es/mora/                      |
-   |                                                                           |
-   |   Copyright (C) 2010  University of Malaga                                |
-   |                                                                           |
-   |    This software was written by the Machine Perception and Intelligent    |
-   |      Robotics (MAPIR) Lab, University of Malaga (Spain).                  |
-   |    Contact: Jose-Luis Blanco  <jlblanco@ctima.uma.es>                     |
-   |                                                                           |
-   |  This file is part of the MORA project.                                   |
-   |                                                                           |
-   |     MORA is free software: you can redistribute it and/or modify          |
-   |     it under the terms of the GNU General Public License as published by  |
-   |     the Free Software Foundation, either version 3 of the License, or     |
-   |     (at your option) any later version.                                   |
-   |                                                                           |
-   |   MORA is distributed in the hope that it will be useful,                 |
-   |     but WITHOUT ANY WARRANTY; without even the implied warranty of        |
-   |     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         |
-   |     GNU General Public License for more details.                          |
-   |                                                                           |
-   |     You should have received a copy of the GNU General Public License     |
-   |     along with MORA.  If not, see <http://www.gnu.org/licenses/>.         |
-   |                                                                           |
    +---------------------------------------------------------------------------+ */
 
+#include "CPFLocalizationApp.h" // User C++ app class 
 
-#include "CPFLocalizationApp.h"
+#define MORA_APP_CLASS         CPFLocalizationApp     
+#define MORA_APP_NAME          "LocalizationPF"    // Default MOOSApp app name
 
-int main(int argc ,char * argv[])
-{
-	try
-	{
-		const char * sMissionFile = "Mission.moos";
-		const char * sMOOSName = "pLocalizationPF";
-
-
-		switch(argc)
-		{
-		case 3:
-			sMOOSName = argv[2];
-		case 2:
-			sMissionFile = argv[1];
-		}
-
-		CPFLocalizationApp TheApp;
-		TheApp.Run(sMOOSName,sMissionFile);
-
-		return 0;
-    }
-	catch (std::exception &e)
-	{
-		std::cerr << "**ERROR** " << e.what() << std::endl;
-		return MOOSFail( "Closing due to an exception." );
-	}
-	catch (...)
-	{
-		return MOOSFail( "Closing due to an untyped exception." );
-	}
-}
+#include <mora_main.h>
 
